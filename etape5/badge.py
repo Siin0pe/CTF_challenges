98305a9933afb383902c01e5c6c93cdfb02b1189
Angerenage <ange.renage@gmail.com>
2025-12-10 11:54:58 +0100

    Ajout de l'étape 5

---
+++ etape5/badge.py
@@ -0,0 +1,76 @@
+# ----------------------------------------------------------
+# NE PAS MODIFIER LES ZONES MARQUÉES "NE PAS MODIFIER"
+# Vous ne devez corriger que les zones marquées "À COMPLÉTER".
+#
+# Le checker vous dira pour chaque étape :
+# - si votre réponse est correcte ou non
+# - ce qu'il attend exactement
+# - des exemples d'entrées / sorties
+#
+# À vous de modifier uniquement le code demandé pour que
+# chaque étape fonctionne comme le checker le décrit.
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# Variable globale utilisée par etape_1
+# ----------------------------------------------------------
+version = 1234
+
+
+# ----------------------------------------------------------
+# Étape 1
+# Cette étape doit renvoyer la variable "version".
+# ----------------------------------------------------------
+def etape_1():
+    # À COMPLÉTER
+    return 0
+
+
+# ----------------------------------------------------------
+# Étape 2
+# Cette fonction reçoit un nombre a en paramètre.
+# ----------------------------------------------------------
+def etape_2(a):
+    # À COMPLÉTER
+    return 0
+
+
+# ----------------------------------------------------------
+# Étape 3
+# Cette fonction reçoit deux nombres a et b.
+# ----------------------------------------------------------
+def etape_3(a, b):
+    # À COMPLÉTER
+    return 0
+
+
+# ----------------------------------------------------------
+# Étape 4
+# Cette fonction reçoit une valeurs : niveau.
+# ----------------------------------------------------------
+def etape_4(niveau):
+    # À COMPLÉTER
+    return "TODO"
+
+
+# ----------------------------------------------------------
+# NE PAS MODIFIER LE CODE CI-DESSOUS
+# ----------------------------------------------------------
+
+# ---- Étape 1 : pas d'entrée, juste un résultat ----
+print(etape_1())
+
+# ---- Étape 2 : une entrée, un résultat ----
+val2 = int(input())
+print(etape_2(val2))
+
+# ---- Étape 3 : deux entrées, un résultat ----
+a = int(input())
+b = int(input())
+print(etape_3(a, b))
+
+# ---- Étape 4 : une entrées, un résultat ----
+niveau = int(input())
+print(etape_4(niveau))
+
